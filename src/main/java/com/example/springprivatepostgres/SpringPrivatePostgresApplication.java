aa9ef63193ce51c0c9a39e31c0d3f4ddee45d9e5
Collin Smith <collin.smith@gmail.com>
2024-03-19 22:41:29 -0600

    Initial checkin for a Spring Boot application interacting with a Private RDS Postgres instance with Secrets Manager in a 3 AZ VPC

---
+++ src/main/java/com/example/springprivatepostgres/SpringPrivatePostgresApplication.java
@@ -0,0 +1,13 @@
+package com.example.springprivatepostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPrivatePostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPrivatePostgresApplication.class, args);
+	}
+
+}
